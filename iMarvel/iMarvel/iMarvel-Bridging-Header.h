bfb6779ee0f9db1639348d4bd4c8112940ba55a5
Ricardo Casanova <ricardo@pijp.eu>
2018-12-16 23:52:48 +0100

    Add iMarvel-Bridging-Header file

---
+++ iMarvel/iMarvel/iMarvel-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  iMarvel-Bridging-Header.h
+//  iMarvel
+//
+//  Created by Ricardo Casanova on 16/12/2018.
+//  Copyright © 2018 Wallapop. All rights reserved.
+//
+
+#ifndef iMarvel_Bridging_Header_h
+#define iMarvel_Bridging_Header_h
+
+//__ Reachability
+#import "Reachability.h"
+
+#endif /* iMarvel_Bridging_Header_h */
