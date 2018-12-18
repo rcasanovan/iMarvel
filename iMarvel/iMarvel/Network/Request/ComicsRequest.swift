28feac9506071f4e96f900beacd05e9aaff7bf68
Ricardo Casanova <ricardo@pijp.eu>
2018-12-18 19:30:58 +0100

    Add ComicsRequest

---
+++ iMarvel/iMarvel/Network/Request/ComicsRequest.swift
@@ -0,0 +1,27 @@
+//
+//  ComicsRequest.swift
+//  iMarvel
+//
+//  Created by Ricardo Casanova on 18/12/2018.
+//  Copyright © 2018 Wallapop. All rights reserved.
+//
+
+import Foundation
+
+struct ComicsRequest: RequestProtocol {
+    
+    typealias ResponseType = ComicsResponse
+    var completion: ((Result<ComicsResponse?>) -> Void)?
+    var method: HTTPMethod = .get
+    var url: URL? = nil
+    var encodableBody: Encodable?
+    var simulatedResponseJSONFile: String?
+    var verbose: Bool?
+    var contentType: ContentType = .json
+    var processHeader: Bool? = false
+    
+    init(nameStartsWith: String?, limit: UInt, offset: UInt) {
+        url = Endpoint.getCharactersWith(nameStartsWith:nameStartsWith, limit: limit, offset: offset).url
+    }
+    
+}
