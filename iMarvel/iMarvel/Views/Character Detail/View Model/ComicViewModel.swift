3d4aa68ad29ff0563ab1833cc3825568af4bbaeb
Ricardo Casanova <ricardo@pijp.eu>
2018-12-19 12:12:26 +0100

    Add urlDetail to ComicViewModel

--- iMarvel/iMarvel/Views/Character Detail/View Model/ComicViewModel.swift
+++ iMarvel/iMarvel/Views/Character Detail/View Model/ComicViewModel.swift
@@ -8,18 +8,27 @@
 
 import Foundation
 
+enum UrlType: String {
+    case detail = "detail"
+    case purchase = "purchase"
+    case reader = "reader"
+    case inAppLink = "inAppLink"
+}
+
 struct ComicViewModel {
     
     let id: Int32
     let title: String
     let description: String?
     let urlImage: URL?
+    let urlDetail: URL?
     
-    init(id: Int32, title: String, description: String?, urlImage: URL?) {
+    init(id: Int32, title: String, description: String?, urlImage: URL?, urlDetail: URL?) {
         self.id = id
         self.title = title
         self.description = description
         self.urlImage = urlImage
+        self.urlDetail = urlDetail
     }
     
     public static func getViewModelsWith(comics: [ComicResponse]) -> [ComicViewModel] {
@@ -29,7 +38,26 @@ struct ComicViewModel {
     public static func getViewModelWith(comic: ComicResponse) -> ComicViewModel {
         let urlImage = ImageManager.shared.getPortraitUrlWith(comic.thumbnail?.path, ext: comic.thumbnail?.ext)
         
-        return ComicViewModel(id: comic.id, title: comic.title, description: comic.description, urlImage: urlImage)
+        let urlDetail = getUrlDetailForType(.detail, urls: comic.urls)
+        
+        return ComicViewModel(id: comic.id, title: comic.title, description: comic.description, urlImage: urlImage, urlDetail: urlDetail)
+    }
+    
+}
+
+extension ComicViewModel {
+    
+    private static func getUrlDetailForType(_ type: UrlType, urls: [UrlResponse]?) -> URL? {
+        guard let urls = urls else {
+            return nil
+        }
+        
+        let urlsResponse = urls.filter { $0.type == type.rawValue }
+        guard let urlResponse = urlsResponse.first else {
+            return nil
+        }
+        
+        return URL(string: urlResponse.url)
     }
     
 }
